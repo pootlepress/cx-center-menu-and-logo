72cce6c7c6fdf8ece443c31053a11c9ea21467e9
anathan2586 <jenfoong@gmail.com>
2014-05-21 16:00:58 +0800

    add check

--- pootlepress-center-mnl-functions.php
+++ pootlepress-center-mnl-functions.php
@@ -1,19 +1,39 @@
 <?php
+$health = 'ok';
+
 if (!function_exists('check_main_heading')) {
-	function check_main_heading() {
-		$options = get_option('woo_template');
-		if (!in_array("Canvas Extensions", $options)) {
-			function woo_options_add($options){
-				$i = count($options);
-				$options[$i++] = array(
-						'name' => __('Canvas Extensions', 'pootlepress-canvas-extensions' ), 
-						'icon' => 'favorite', 
-						'type' => 'heading'
-						);
-				return $options;
-			}
-		}
-	}
+    function check_main_heading() {
+        global $health;
+        if (!function_exists('woo_options_add') ) {
+            function woo_options_add($options) {
+                $cx_heading = array( 'name' => __('Canvas Extensions', 'pootlepress-canvas-extensions' ),
+                    'icon' => 'favorite', 'type' => 'heading' );
+                if (!in_array($cx_heading, $options))
+                    $options[] = $cx_heading;
+                return $options;
+            }
+        } else {	// another ( unknown ) child-theme or plugin has defined woo_options_add
+            $health = 'ng';
+        }
+    }
+}
+
+add_action( 'admin_init', 'poo_commit_suicide' );
+
+if(!function_exists('poo_commit_suicide')) {
+    function poo_commit_suicide() {
+        global $health;
+        $pluginFile = str_replace('-functions', '', __FILE__);
+        $plugin = plugin_basename($pluginFile);
+        $plugin_data = get_plugin_data( $pluginFile, false );
+        if ( $health == 'ng' && is_plugin_active($plugin) ) {
+            deactivate_plugins( $plugin );
+            wp_die( "ERROR: <strong>woo_options_add</strong> function already defined by another plugin. " .
+                $plugin_data['Name']. " is unable to continue and has been deactivated. " .
+                "<br /><br />Please contact PootlePress at <a href=\"mailto:support@pootlepress.com?subject=Woo_Options_Add Conflict\"> support@pootlepress.com</a> for additional information / assistance." .
+                "<br /><br />Back to the WordPress <a href='".get_admin_url(null, 'plugins.php')."'>Plugins page</a>." );
+        }
+    }
 }
 
 	/* Centre logo */
